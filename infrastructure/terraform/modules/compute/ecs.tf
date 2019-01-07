dd4d5978fa95051e15768246510f0a96e6837c67
Alex Baily <alex.d.baily@gmail.com>
2019-01-07 21:03:15 +0000

    Add ECS branch with ECS Task Definitions and clusters for web

---
+++ infrastructure/terraform/modules/compute/ecs.tf
@@ -0,0 +1,32 @@
+#ECS Prod Cluster
+resource "aws_ecs_cluster" "prod_cluster" {
+  name = "prod-cluster"
+}
+
+#ECS Web Task Definition
+resource "aws_ecs_task_definition" "web" {
+  family                = "service"
+  container_definitions = "${file("../../modules/compute/files/task-definitions/web.json")}"
+
+}
+
+resource "aws_ecs_service" "web" {
+  name            = "web"
+  cluster         = "${aws_ecs_cluster.prod_cluster.id}"
+  task_definition = "${aws_ecs_task_definition.web.arn}"
+  desired_count   = 2
+  iam_role        = "${aws_iam_role.foo.arn}"
+  depends_on      = ["aws_iam_role_policy."]
+
+  ordered_placement_strategy {
+    type  = "binpack"
+    field = "cpu"
+  }
+
+  network_configuration {
+    subnets = "${var.
+
+  }
+
+}
+
