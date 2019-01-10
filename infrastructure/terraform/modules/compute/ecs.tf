074ff0045072a566a5b5f834b239bf8d0e26e810
Alex Baily <alex.d.baily@gmail.com>
2019-01-10 21:54:06 +0000

    Add ecs profile and role for consul agent retry-join provider

--- infrastructure/terraform/modules/compute/ecs.tf
+++ infrastructure/terraform/modules/compute/ecs.tf
@@ -42,11 +42,12 @@ data "aws_ami" "ecs_server_ami" {
 
 
 resource "aws_launch_configuration" "ecs_as_conf" {
-  image_id        = "${data.aws_ami.ecs_server_ami.id}"
-  instance_type   = "t2.medium"
-  key_name        = "kops-ssh-key"
-  user_data       = "${file("../../modules/compute/files/ecs-user-data.sh")}"
-  security_groups = ["${aws_security_group.ecs_server.id}", "${aws_security_group.consul_agent.id}"]
+  image_id             = "${data.aws_ami.ecs_server_ami.id}"
+  instance_type        = "t2.medium"
+  key_name             = "kops-ssh-key"
+  user_data            = "${file("../../modules/compute/files/ecs-user-data.sh")}"
+  iam_instance_profile = "${aws_iam_instance_profile.ecs-profile.name}"
+  security_groups      = ["${aws_security_group.ecs_server.id}", "${aws_security_group.consul_agent.id}"]
   lifecycle { create_before_destroy = true }
 }
 
@@ -61,3 +62,56 @@ resource "aws_autoscaling_group" "ecs_as_group" {
   lifecycle { create_before_destroy = true }
 }
 
+############
+#ECS Role  #
+###########
+
+resource "aws_iam_role" "ecs-role" {
+  name = "ecs-role"
+
+  assume_role_policy = <<EOF
+{
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "ec2.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        }
+      ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "ecs-policy" {
+  name        = "ecs-policy"
+  description = "ECS Policy for describing instances."
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:DescribeInstances"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-attach" {
+  role       = "${aws_iam_role.ecs-role.name}"
+  policy_arn = "${aws_iam_policy.ecs-policy.arn}"
+}
+
+resource "aws_iam_instance_profile" "ecs-profile" {
+  name = "ecs-profile"
+  role = "${aws_iam_role.ecs-role.name}"
+}
