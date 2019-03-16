98368953b594e62a0ef898b836fcd592bcb08679
Alex Baily <alex.d.baily@gmail.com>
2019-03-16 11:34:59 +0000

    Add KOPs IAM role so that it has access to create the clusters.

--- infrastructure/terraform/modules/compute/kubernetes.tf
+++ infrastructure/terraform/modules/compute/kubernetes.tf
@@ -23,7 +23,8 @@ resource "aws_security_group" "kops_master" {
 resource "aws_launch_configuration" "as_conf" {
   name            = "${var.name} launch configuration"
   image_id        = "${data.aws_ami.kops_master.id}"
-  instance_type   = "t2.nano"
+  instance_type   = "t2.nano",
+  iam_instance_profile = "${aws_iam_instance_profile.kops-profile.name}",
   key_name        = "kops-ssh-key"
   security_groups = ["${aws_security_group.kops_master.id}"]
 }
@@ -36,4 +37,63 @@ resource "aws_autoscaling_group" "as_group" {
   health_check_grace_period = 300
   health_check_type         = "EC2"
   launch_configuration      = "${aws_launch_configuration.as_conf.name}"
+}
+
+
+###############
+#KOPS IAM Role#
+###############
+
+resource "aws_iam_role" "kops-role" {
+  name = "kops-role"
+
+  assume_role_policy = <<EOF
+{
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "ec2.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        }
+      ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "kops-policy" {
+  name        = "kops-policy"
+  description = "KOPS Policy for required access."
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:*",
+        "route53:*",
+        "iam:*",
+        "s3:*",
+        "vpc:*"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "kops-attach" {
+  role       = "${aws_iam_role.kops-role.name}"
+  policy_arn = "${aws_iam_policy.kops-policy.arn}"
+}
+
+resource "aws_iam_instance_profile" "kops-profile" {
+  name = "kops-profile"
+  role = "${aws_iam_role.kops-role.name}"
 }
